2d4cfe1fcd1f934e90ee0b7052748abd43695f70
Marcin Kwiatkowski <kwiatkowski939@gmail.com>
2015-09-30 08:32:53 +0200

    Add styles for carousel component

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+(function($) {
+  var project = {};
+  project.$carousel = $('[data-component=carousel]');
+  project.initCarousel = function($carousel, options) {
+    console.log('works')
+    if($.fn.slick && $carousel.length) {
+      return function() {
+        $carousel.slick(options);
+      }
+    }
+
+  };
+  project.init = function() {
+    var initCarousel = project.initCarousel(project.$carousel, {
+      mobileFirst: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      variableWidth: true
+      //centerPadding: '20px'
+    });
+
+    $(window).on('load', initCarousel);
+  };
+
+  $(project.init);
+})(jQuery);
