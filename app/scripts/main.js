f4f09a8826b153211dd62d93d380409f6b5b068b
Marcin Kwiatkowski <kwiatkowski939@gmail.com>
2015-10-01 21:21:02 +0200

    Add styles for testimonials section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,26 @@
 (function($) {
   var project = {};
   project.$carousel = $('[data-component=carousel]');
+  project.$slider = $('[data-component=slider]');
   project.initCarousel = function($carousel, options) {
-    console.log('works')
     if($.fn.slick && $carousel.length) {
-      return function() {
-        $carousel.slick(options);
-      }
+      $carousel.slick(options);
     }
 
   };
   project.init = function() {
-    var initCarousel = project.initCarousel(project.$carousel, {
-      mobileFirst: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      centerMode: true,
-      variableWidth: true
-      //centerPadding: '20px'
-    });
+    $(window).on('load', function() {
+      project.initCarousel(project.$carousel, {
+        mobileFirst: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true
+        //centerPadding: '20px'
+      });
 
-    $(window).on('load', initCarousel);
+      project.initCarousel(project.$slider);
+    });
   };
 
   $(project.init);
